77f85edb4990b99009292cea1b3baf0e533485ab
Mark Jordan Ugtong <159249841+markjordanugtongspc@users.noreply.github.com>
2025-06-03 10:41:29 +0800

    Modified auth_api.js
    Remove API Key

--- jscript/auth_api.js
+++ jscript/auth_api.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ovizwwfnkdckcislqupr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92aXp3d2Zua2Rja2Npc2xxdXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3ODYwNzEsImV4cCI6MjA2MjM2MjA3MX0.6HDljFFGIW-K_DR_ZwEZj6jZbL8jpKxOxOM1whpqrK0'; // Your anon public key
+const supabaseKey = ''; // Your anon public key
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function signUpUser(email, hashedPassword) {
